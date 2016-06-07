e641974c5ea659c7e2d76f8a6ec4a50dbd49280b
Steve Lasker <StevenLasker@Hotmail.com>
2016-06-06 21:54:27 -0700

    Moved the API Project to it's own repo

---
+++ src/MultiService.API/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiService.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Models.Customer> Get()
+        {
+            Models.Customer[] customers = new Models.Customer[] {
+                new Models.Customer("Foo", "Freddy Foo"),
+                new Models.Customer("Bar", "Jason Bar")
+            };
+            return customers;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Models.Customer Get(string id)
+        {
+            return new Models.Customer("Foo", "Freddy Foo");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/MultiService.API/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiService.API.Models
+{
+    public class Customer
+    {
+        public Customer(string customerId, string name)
+        {
+            this.CustomerId = customerId;
+            this.Name = name;
+        }
+        public string CustomerId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
